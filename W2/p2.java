486490442e844d8830aa11999fd5c45c7dd3b5e8
Kyle8Bit <khoamaidang2611@gmail.com>
2024-03-16 20:02:29 +0700

    week2

---
+++ W2/p2.java
@@ -0,0 +1,20 @@
+public class p2{
+    public static void main (String[] args){
+        
+        int [] Array = {6,8,10,11,6,10};
+        boolean found;
+        for (int i = 0; i < Array.length; i++){
+            found = false;
+            for (int j = 0; j < Array.length; j++){
+                if (i!=j && Array[i] == Array[j]){
+                    found = true;
+                    break;
+                }
+            }
+            if(!found){
+                System.out.println(Array[i]);
+            }
+        }
+    
+    }
+}
---
+++ W2/p4.java
@@ -0,0 +1,7 @@
+public class p4 {
+    public static void main(String[] args) {
+        int[] A = { 2, 3, 6, 9 };
+        long[] B = { -100, 50, -29, 99 };
+
+    }
+}
